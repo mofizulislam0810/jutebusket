259ea86e2423fa3108e2e5d424cae77b868bde33
mofizulislam0810 <mofizul.islam0810@gmail.com>
2022-07-27 22:38:04 +0600

    27-07-2022

--- dbconnect.php
+++ dbconnect.php
@@ -1,4 +1,4 @@
 <?php 
-$connection = mysqli_connect("localhost","root","0810","jute") or
+$connection = mysqli_connect("sql203.epizy.com","epiz_32258505","2k3UgVHX2M5Vg","epiz_32258505_jute") or
 die("Error".mysqli_error($connection));
 ?>
